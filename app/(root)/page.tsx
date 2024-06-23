5817a488a43ab5e2c207e4983df128995f01daee
EhsaasChaudhary <116794955+EhsaasChaudhary@users.noreply.github.com>
2024-06-23 20:38:51 +0530

    added card for amount and added a header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
+import HeaderBox from "@/components/ui/headerBox";
 
 const Home = () => {
+  const loggedin = { firstname: "Ehsaas" };
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedin?.firstname || "Guest"}
+            subtext="Access and manage your bank account and transactions with utmost efficiency"
+          />
+        </header>
 
-export default Home
+        <TotalBalanceBox 
+        account={[]}
+        totalBanks={1}
+        totalCurrentBalance={1251.15}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
